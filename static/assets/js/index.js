11c20d7bb5291abecc3bbc5ab6254787bcc90c90
MariamHossam01 <mariam.mostafa01@eng-st.cu.edu.eg>
2022-12-25 11:11:41 +0200

    update image in real time

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -4,41 +4,59 @@ const cropper = new Cropper(image, { zoomable: 0, aspectRatio: 0, viewMode: 0, }
 const cropper2 = new Cropper(image2, { zoomable: 0, aspectRatio: 0, viewMode: 0, });
 
 // document.getElementById('cropImageBtn').addEventListener('click', function () {
+
     document.getElementById('pic1').addEventListener('click', function () {
-        var croppedImage = cropper.getCroppedCanvas().toDataURL('image/png');
-        var croppedImage2 = cropper2.getCroppedCanvas().toDataURL('image/png');
-        var data2 = cropper2.getCropBoxData();
-        var data = cropper.getCropBoxData();
-        // var pictures={"pic1":data,"pic2":data2}
-        var pic1_choice= document.querySelector( 'input[name="pic1_radio"]:checked'); 
-        if(pic1_choice != null) {pic1_choice= pic1_choice.value; }
-        var pic2_choice= document.querySelector( 'input[name="pic2_radio"]:checked'); 
-        if(pic2_choice != null) {pic2_choice= pic2_choice.value; }
-        var pictures={"pic1":data,"pic2":data2,"pic1_choice":pic1_choice,"pic2_choice":pic2_choice};
-        // document.getElementById('output').src = croppedImage;
-        $.ajax({
-            url:"/crop_image1",
-            type:"POST",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(pictures)});
+
+        images_processing()
     });
+
+
     // document.getElementById('pic2').addEventListener('click', function () {
     document.getElementById('pic2').addEventListener('click', function () {
-        var croppedImage = cropper.getCroppedCanvas().toDataURL('image/png');
-        var croppedImage2 = cropper2.getCroppedCanvas().toDataURL('image/png');
+
+        images_processing()
+    }); 
+
+    function images_processing()
+    {
+        // var croppedImage = cropper.getCroppedCanvas().toDataURL('image/png');
+        // var croppedImage2 = cropper2.getCroppedCanvas().toDataURL('image/png');
+
         var data2 = cropper2.getCropBoxData();
         var data = cropper.getCropBoxData();
         
         var pic1_choice= document.querySelector( 'input[name="pic1_radio"]:checked'); 
-        if(pic1_choice != null) {pic1_choice= pic1_choice.value; }
         var pic2_choice= document.querySelector( 'input[name="pic2_radio"]:checked'); 
+
+        if(pic1_choice != null) {pic1_choice= pic1_choice.value; }
+        else {pic1_choice= 0 }
+
         if(pic2_choice != null) {pic2_choice= pic2_choice.value; }
+        else {pic1_choice= 1 }
+
+
         var pictures={"pic1":data,"pic2":data2,"pic1_choice":pic1_choice,"pic2_choice":pic2_choice};
-        // document.getElementById('output').src = croppedImage;
+        
         
         $.ajax({
             url:"/crop_image1",
             type:"POST",
             contentType: "application/json",
-            data: JSON.stringify(pictures)});
-    });
+            data: JSON.stringify(pictures),
+            success: function(){
+                var d = new Date(); 
+                var image1="../static/assets/images/outputs/output_image1.png?"+d.getMilliseconds(); 
+                var image2="../static/assets/images/outputs/output_image2.png?"+d.getMilliseconds();
+        
+                $(".img1").attr("src",image1);
+                $(".img2").attr("src",image2);
+            }
+        });
+
+        // var d = new Date(); 
+        // var image="../static/assets/images/outputs/output_image1.png?"+d.getMilliseconds(); //pic.src(image);
+        // document.getElementById('output1').src = image;
+        // document.getElementById('output2').src = "../static/assets/images/outputs/output_image2.png?random";
+        
+        // $(".img2").attr("src",image);
+    }
