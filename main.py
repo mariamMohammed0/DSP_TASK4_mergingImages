7d637521e63c9d61c9fe7fd10d084a668ec46af5
Abram1111 <ebram.meawad01@eng-st.cu.edu.eg>
2022-12-26 18:06:05 +0200

    index

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, send_file, request, redirect,jsonify
-import functions as fn 
+from flask import Flask, render_template, send_file, request, redirect, jsonify
+import functions as fn
 import numpy as np
 
 
@@ -8,29 +8,38 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    cropped_indecies=np.zeros(8)
-    fn.read_2images(cropped_indecies) 
+    cropped_indecies = np.zeros(8)
+    fn.read_2images(cropped_indecies)
 
     return render_template('index.html')
-        
+
+
 @app.route('/crop_image1', methods=['POST'])
 def crop_image1():
     output = request.get_json()
 
-    cropped_indecies=[0,0,0,0,0,0,0,0]
-    cropped_indecies[0]=int(round(output['pic1']['left']))
-    cropped_indecies[1]=int(round(output['pic1']['top'] ))
-    cropped_indecies[2]=int(round(output['pic1']['width']))
-    cropped_indecies[3]=int(round(output['pic1']['height']))
+    cropped_indecies = [0, 0, 0, 0, 0, 0, 0, 0]
+    cropped_indecies[0] = int(round(output['pic1']['left']))
+    cropped_indecies[1] = int(round(output['pic1']['top']))
+    cropped_indecies[2] = int(round(output['pic1']['width']))
+    cropped_indecies[3] = int(round(output['pic1']['height']))
 
-    cropped_indecies[4]=int(round(output['pic2']['left']))
-    cropped_indecies[5]=int(round(output['pic2']['top'] ))
-    cropped_indecies[6]=int(round(output['pic2']['width']))
-    cropped_indecies[7]=int(round(output['pic2']['height']))
+    cropped_indecies[4] = int(round(output['pic2']['left']))
+    cropped_indecies[5] = int(round(output['pic2']['top']))
+    cropped_indecies[6] = int(round(output['pic2']['width']))
+    cropped_indecies[7] = int(round(output['pic2']['height']))
 
     fn.read_2images(cropped_indecies)
     return output
 
+
+@app.route('/save_image', methods=['POST'])
+def save_image():
+    print('---------------------------------------------')
+    output = request.get_json()
+    print('---------------------------------------------')
+    print(output)
+    return output
 # @app.route('/crop_image2', methods=['POST'])
 # def crop_image2():
 #     output = request.get_json()
@@ -49,6 +58,7 @@ def crop_image1():
 #     # fn.read_2images(cropped_indecies)
 #     return output
 
+
 if __name__ == "__main__":
-    
+
     app.run(debug=True, threaded=True)
--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -5,58 +5,74 @@ const cropper2 = new Cropper(image2, { zoomable: 0, aspectRatio: 0, viewMode: 0,
 
 // document.getElementById('cropImageBtn').addEventListener('click', function () {
 
-    document.getElementById('pic1').addEventListener('click', function () {
+document.getElementById('pic1').addEventListener('click', function () {
 
-        images_processing()
+    images_processing()
+});
+
+
+// document.getElementById('pic2').addEventListener('click', function () {
+document.getElementById('pic2').addEventListener('click', function () {
+
+    images_processing()
+});
+
+function images_processing() {
+    // var croppedImage = cropper.getCroppedCanvas().toDataURL('image/png');
+    // var croppedImage2 = cropper2.getCroppedCanvas().toDataURL('image/png');
+
+    var data2 = cropper2.getCropBoxData();
+    var data = cropper.getCropBoxData();
+
+    var pic1_choice = document.querySelector('input[name="pic1_radio"]:checked');
+    var pic2_choice = document.querySelector('input[name="pic2_radio"]:checked');
+
+    if (pic1_choice != null) { pic1_choice = pic1_choice.value; }
+    else { pic1_choice = 0 }
+
+    if (pic2_choice != null) { pic2_choice = pic2_choice.value; }
+    else { pic1_choice = 1 }
+
+
+    var pictures = { "pic1": data, "pic2": data2, "pic1_choice": pic1_choice, "pic2_choice": pic2_choice };
+
+
+    $.ajax({
+        url: "/crop_image1",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(pictures),
+        success: function () {
+            var d = new Date();
+            var image1 = "../static/assets/images/outputs/output_image1.png?" + d.getMilliseconds();
+            var image2 = "../static/assets/images/outputs/output_image2.png?" + d.getMilliseconds();
+
+            $(".img1").attr("src", image1);
+            $(".img2").attr("src", image2);
+        }
+    });
+
+    // var d = new Date(); 
+    // var image="../static/assets/images/outputs/output_image1.png?"+d.getMilliseconds(); //pic.src(image);
+    // document.getElementById('output1').src = image;
+    // document.getElementById('output2').src = "../static/assets/images/outputs/output_image2.png?random";
+
+    // $(".img2").attr("src",image);
+}
+const upload_btn = document.getElementById('image_uploads');
+upload_btn.addEventListener('change', updateImageDisplay);
+function updateImageDisplay() {
+
+    const curFiles = upload_btn.files;
+    file_name = curFiles[0].name;
+    console.log(curFiles[0].name)
+
+    $.ajax({
+        url: "/save_image",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(file_name),
     });
 
 
-    // document.getElementById('pic2').addEventListener('click', function () {
-    document.getElementById('pic2').addEventListener('click', function () {
-
-        images_processing()
-    }); 
-
-    function images_processing()
-    {
-        // var croppedImage = cropper.getCroppedCanvas().toDataURL('image/png');
-        // var croppedImage2 = cropper2.getCroppedCanvas().toDataURL('image/png');
-
-        var data2 = cropper2.getCropBoxData();
-        var data = cropper.getCropBoxData();
-        
-        var pic1_choice= document.querySelector( 'input[name="pic1_radio"]:checked'); 
-        var pic2_choice= document.querySelector( 'input[name="pic2_radio"]:checked'); 
-
-        if(pic1_choice != null) {pic1_choice= pic1_choice.value; }
-        else {pic1_choice= 0 }
-
-        if(pic2_choice != null) {pic2_choice= pic2_choice.value; }
-        else {pic1_choice= 1 }
-
-
-        var pictures={"pic1":data,"pic2":data2,"pic1_choice":pic1_choice,"pic2_choice":pic2_choice};
-        
-        
-        $.ajax({
-            url:"/crop_image1",
-            type:"POST",
-            contentType: "application/json",
-            data: JSON.stringify(pictures),
-            success: function(){
-                var d = new Date(); 
-                var image1="../static/assets/images/outputs/output_image1.png?"+d.getMilliseconds(); 
-                var image2="../static/assets/images/outputs/output_image2.png?"+d.getMilliseconds();
-        
-                $(".img1").attr("src",image1);
-                $(".img2").attr("src",image2);
-            }
-        });
-
-        // var d = new Date(); 
-        // var image="../static/assets/images/outputs/output_image1.png?"+d.getMilliseconds(); //pic.src(image);
-        // document.getElementById('output1').src = image;
-        // document.getElementById('output2').src = "../static/assets/images/outputs/output_image2.png?random";
-        
-        // $(".img2").attr("src",image);
-    }
+}
